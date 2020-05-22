6629b6b38de085c04acbf817e72d3c700d7f6e9c
adiisanjaya12 <61811876+adiisanjaya12@users.noreply.github.com>
2020-05-22 15:57:04 +0800

    react part 1 dan 2

---
+++ react part 2/src/components/appointment/list.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class list extends Component {
+    render()  {
+      return  (
+          <div>
+              <h3>Ini list</h3>
+          </div>
+        )
+    }
+}
+
+export default list
---
+++ react part 2/src/components/appointment/today.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class today extends Component {
+    render()  {
+      return  (
+          <div>
+              <h3>Ini today</h3>
+          </div>
+        )
+    }
+}
+
+export default today
