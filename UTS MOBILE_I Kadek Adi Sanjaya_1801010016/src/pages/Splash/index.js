a6dd0073ae115b41ccbb18011fc1bed8b4e94bcf
adiisanjaya12 <61811876+adiisanjaya12@users.noreply.github.com>
2020-11-29 22:47:32 +0800

    splash page

--- UTS MOBILE_I Kadek Adi Sanjaya_1801010016/src/pages/Splash/index.js
+++ UTS MOBILE_I Kadek Adi Sanjaya_1801010016/src/pages/Splash/index.js
@@ -1,17 +1,48 @@
 import React, { useEffect } from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
+import { welcomeAuth } from '../../assets';
+import { colors } from '../../utils';
 
 const Splash = ({navigation}) => {
     useEffect(() => {
         setTimeout(() => {
             navigation.replace('WelcomeAuth');
-        }, 2000);
+        }, 3000);
     });
-    return (
-        <View>
-            <Text>Splash Screen</Text>
+
+
+   return (
+        <View style={styles.wrapper.page}>
+            <Image source={welcomeAuth} style={styles.wrapper.illustration}/>
+            <Text style={styles.text.welcome}>
+            Selamat Datang di O-jol
+            </Text>
         </View>
     );
 };
 
+const styles = {
+    wrapper: {
+        page: {
+            alignItems: 'center', 
+            justifyContent: 'center',
+            flex: 1,
+            backgroundColor: 'white',
+        },
+        illustration: {
+                width: 160, 
+                height: 150, 
+                marginBottom: 5,
+                },
+        },
+
+        text: {
+            welcome: {
+                    fontSize: 18, 
+                    fontWeight: 'bold', 
+                    color: colors.default, 
+                    marginBottom: 76,
+        },
+    },
+};
 export default Splash;
